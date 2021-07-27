d14140cbd44eb4f3f0a1dfef0199227d623b958c
Ruan.Miniguite <ruanminiguite@gmail.com>
2021-07-27 00:33:18 -0300

    :hammer: Zoom e centralização do map

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const api_url_id = 'https://api.wheretheiss.at/v1/satellites/25544';
 const api_url_name = 'http://api.open-notify.org/astros.json';
 
+const ISSIcon = L.icon({
+    iconUrl: "imagens/ISS.svg",
+    iconSize: [50, 32],
+    iconAnchor: [25, 36],
+});
 
-const mymap = L.map('mapBg').setView([51.505, -0.09], 2);
-const attrribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const mymap = L.map('mapBg').setView([0, 0], 2);
+const marker =  L.marker([0, 0], {icon: ISSIcon}).addTo(mymap);
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, { attrribution });
+const tiles = L.tileLayer(tileUrl, { attribution });
 
 tiles.addTo(mymap);
 
-
 async function getISS() {
-    const response = await fetch(api_url_id)
-    const data = await response.json()
+    const response = await fetch(api_url_id);
+    const data = await response.json();
     const {
         altitude,
         latitude,
@@ -24,27 +30,28 @@ async function getISS() {
 
     var time =  new Date(timestamp * 1000);
 
-    document.getElementById('latitude').textContent = latitude
-    document.getElementById('longitude').textContent = longitude
-    document.getElementById('altitude').textContent = altitude.toFixed(2)
-    document.getElementById('velocidade').textContent = velocity.toFixed(2) + " km/h"
+    marker.setLatLng([latitude, longitude]);
+    mymap.setView([latitude, longitude], 3)
+
+    document.getElementById('latitude').textContent = latitude;
+    document.getElementById('longitude').textContent = longitude;
+    document.getElementById('altitude').textContent = altitude.toFixed(2);
+    document.getElementById('velocidade').textContent = velocity.toFixed(2) + " km/h";
     document.getElementById('timestamp').textContent =  time.toLocaleString();
     
-    console.log(visibility)
-    getPeople()
+    console.log(visibility);
+    getPeople();
 }
-
-
-
-
 
 async function getPeople(){
-    const response = await fetch(api_url_name)
-    const people = await response.json()
+    const response = await fetch(api_url_name);
+    const people = await response.json();
 
-    let list = people.people
+    let list = people.people;
 
-    document.getElementById('pessoas').textContent = list.filter(x=> x.craft == "ISS").length
+    document.getElementById('pessoas').textContent = list.filter(x=> x.craft == "ISS").length;
 }
 
+getISS();
 
+setInterval(getISS, 1500);
